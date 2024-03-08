f46413f269895bb6f033d42e08aac749cf6ae5f8
damien <damien.clementdhuart@gmail.com>
2024-03-09 00:10:13 +0100

    remove unused code

--- integration-tests/src/test/java/io/quarkiverse/shedlock/it/ShedlockResourceIT.java
+++
@@ -1,7 +0,0 @@
-package io.quarkiverse.shedlock.it;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class ShedlockResourceIT extends ShedlockResourceTest {
-}
